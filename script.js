f131ea970c9e560e2ec9375e7f943a6f38d6e475
LaurenJablonski <44180505+LaurenJablonski@users.noreply.github.com>
2020-07-22 09:37:49 +0100

    added the countdown stuff to inside the submitItems function
    this way I'm not calling a varaible outside of a functionn and it will execute the if or else statements depending on the value

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /**
  * Gets a token to be used in API calls
  * This token references your personal database instance, should be kept consitent throughout the exercise
@@ -6,7 +5,7 @@
  * @returns {string}
  */
 function getToken() {
-  return 'lauren2';
+    return 'lauren2';
 }
 
 /**
@@ -20,18 +19,18 @@ function getToken() {
  * @param {function} errorCb On error callback
  */
 function makeRequest(method, resource, body, successCb, errorCb) {
-  var baseUrl = 'https://todoapi.nxj.io';
-  console.log(JSON.stringify(body));
-  /** alert(method+resource+JSON.stringify(body));*/
-  $.ajax({
-    method: method,
-    url: baseUrl + resource,
-    headers: { 'token': getToken() },
-    data: body ? JSON.stringify(body) : null,
-    success: successCb,
-    error: errorCb
-  });
-  alert('');
+    var baseUrl = 'https://todoapi.nxj.io';
+    console.log(JSON.stringify(body));
+    /** alert(method+resource+JSON.stringify(body));*/
+    $.ajax({
+        method: method,
+        url: baseUrl + resource,
+        headers: {'token': getToken()},
+        data: body ? JSON.stringify(body) : null,
+        success: successCb,
+        error: errorCb
+    });
+    alert('');
 }
 
 /**
@@ -40,13 +39,13 @@ function makeRequest(method, resource, body, successCb, errorCb) {
  * @param {function} callback On success callback, function takes one argument: the item array
  */
 function getItems(callback) {
-  makeRequest('GET', '/item', null, function(data) {
-    var items = data['Data'];
-    callback(items);
-  }, function() {
-    console.log("An error occured in getItems");
-    callback([]);
-  });
+    makeRequest('GET', '/item', null, function (data) {
+        var items = data['Data'];
+        callback(items);
+    }, function () {
+        console.log("An error occured in getItems");
+        callback([]);
+    });
 }
 
 /**
@@ -60,19 +59,23 @@ function getItems(callback) {
  * @param {string} howlong allows the user to easily see how much time they have left to complete the task
  */
 function addItem(name, description, assignee, dueDate, props, howLong) {
-  var body = {'Name': name, 'Desc': description,
-    'Assignee': assignee, 'DueDate': dueDate,
-    'Props': props, 'HowLong': howLong};
-  /** alert(body);*/
-  makeRequest('POST', '/item', body, function(data) {/** It makes the request and if the request is successful then it executes getItems(). If no success then it tells you there's an error*/
-  console.log('success');/** in order to add a new item you must make a POST request */
-    /**alert('success');*/
-    getItems();
-  }, function() {
-    console.log("An error occured in addItem");
-    /** alert("An error occured in addItem");*/
+    var body = {
+        'Name': name, 'Desc': description,
+        'Assignee': assignee, 'DueDate': dueDate,
+        'Props': props, 'HowLong': howLong};
 
-  });
+    /** alert(body);*/
+    makeRequest('POST', '/item', body, function (data) {
+        /** It makes the request and if the request is successful then it executes getItems(). If no success then it tells you there's an error*/
+        console.log('success');/** in order to add a new item you must make a POST request */
+        /**alert('success');*/
+        getItems();
+
+    }, function () {
+        console.log("An error occured in addItem");
+        /** alert("An error occured in addItem");*/
+
+    });
 }
 
 /**
@@ -80,20 +83,56 @@ function addItem(name, description, assignee, dueDate, props, howLong) {
  and sends them to the addItem function and then sends the data to the API server. Note:.value gives you the property of the text inside the
  textbox.
  */
-function submitItem(){
-  let todoItemname = document.getElementById("nameTextboxID").value;
-  let todoItemDesc = document.getElementById("descriptionTextboxID").value;
-  let todoItemDate = document.getElementById("dateTextboxID").value
+function submitItem() {
+    alert("testing");
+    let todoItemname = document.getElementById("nameTextboxID").value;
 
-  let dateToCountdownFrom = new Date(todoItemDate);
-  /**alert(todoItemDate);
-  alert(dateToCountdownFrom);*/
-  x;  /** when the + button is pressed then submitItem is executed which adds the items which needs to add the how long bit too so therefore must ensure that we call the variable x */
-  addItem(todoItemname,todoItemDesc,'',todoItemDate,'',dateToCountdownFrom);
+    let todoItemDesc = document.getElementById("descriptionTextboxID").value;
+    let todoItemDate = document.getElementById("dateTextboxID").value
+
+    let dateToCountdownFrom = new Date(todoItemDate);
+
+    var countDownDate = dateToCountdownFrom.getTime();
+    /** would need to set this to whichever date entered for each item by the user (so would need to make this the user input box on the other page (input id="todoItemDate") i think */
+
+    var x = setInterval(function () {
+        // Set the date we're counting down to
+
+        // Get today's date and time since this will be used to determine the time difference between now and the deadline
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
 
 
+        // Time calculations for just days (thought having the seconds countdown would be a bit intense on a to-do list)
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24)); // Converts distance into days and rounds
+
+        if (days > 2) {
+            // If the deadline is more than 2 days away then just show the number of days
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = days + " days until deadline"
+
+        } else if (days >= 0 && days <= 2) {
+            // When the deadline is within 2 days away then display deadline approaching in orange
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "<span style='color: orange;'>Deadline approaching!</span>";
+
+        } else if (days < 0) {
+            // when the deadline is reached then it displays EXPIRED in red
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "<span style='color: red;'>DEADLINE EXPIRED</span>";
+        }
+
+
+    }, 1000);
+
+
+    /**alert(todoItemDate);
+     alert(dateToCountdownFrom);*/
+    /** x;  when the + button is pressed then submitItem is executed which adds the items which needs to add the how long bit too so therefore must ensure that we call the variable x */
+    addItem(todoItemname, todoItemDesc, '', todoItemDate, '', x);
 }
-
 
 
 /**
@@ -102,9 +141,6 @@ function submitItem(){
  * @param {int} id ID of item
  */
 function deleteItem(id) {
-
-    document.getElementById('ID').remove();
-
 }
 
 /**
@@ -117,7 +153,8 @@ function deleteItem(id) {
  * @param {string} dueDate Due Date of the item
  * @param {string} props Any other information you might want to include
  */
-function updateItem(id, name, description, assignee, dueDate, props, howLong) {}
+function updateItem(id, name, description, assignee, dueDate, props, howLong) {
+}
 
 /**
  * Adds an item list onto the page
@@ -134,20 +171,20 @@ function updateItem(id, name, description, assignee, dueDate, props, howLong) {}
  * If there was only one item in the list then the string would become '<div>ID,Name,Description</div><div>01,todo1,first to do item </div>'.
  */
 function createItemTable(items) {
-  alert(JSON.stringify(items[2]['DueDate']));
-  var list = '<table style="width:100%" position:absolute ><tr><th>ID</th><th>Name</th><th>Description</th><th>Duedate</th><th> How long left?</th></tr>';
-  items.forEach(i => {
-    element = '<div>'
-    element += '<tr><td>' + i['ID'] + '. ' + '</td>';
-    element += '<td>' + i['Name'] + '</td>';
-    element += '<td>' + i['Desc'] + '</td>';
-    element += '<td>' + i['DueDate'] + '</td>';
-    element += '<td>' + i['HowLong'] + '</td>';
-    element += '</div>'
-    list += element
-  });
-  element+='</table>';
-  $('#list').html(list);
+    alert(JSON.stringify(items[2]['countdown']));
+    var list = '<table style="width:100%" position:absolute ><tr><th>ID</th><th>Name</th><th>Description</th><th>Duedate</th><th> How long left?</th></tr>';
+    items.forEach(i => {
+        element = '<div>'
+        element += '<tr><td>' + i['ID'] + '. ' + '</td>';
+        element += '<td>' + i['Name'] + '</td>';
+        element += '<td>' + i['Desc'] + '</td>';
+        element += '<td>' + i['DueDate'] + '</td>';
+        element += '<td>' + i['countdown'] + '</td>';
+        element += '</div>'
+        list += element
+    });
+    element += '</table>';
+    $('#list').html(list);
 
 }
 
@@ -157,50 +194,16 @@ function createItemTable(items) {
  * Pulls all items from the API and displays them on the page
  */
 function refreshList() {
-  getItems(function(items) {
-    createItemTable(items);
-  });
+    getItems(function (items) {
+        createItemTable(items);
+    });
 }
 
-$().ready(function() { //* this function means that when the page has finished loading, it calls the refreshlist function, where this refreshlist function calls the getItems function with fucnction createItemTable as a function. (it's passing a function as an argument) */
-  refreshList();
+$().ready(function () { //* this function means that when the page has finished loading, it calls the refreshlist function, where this refreshlist function calls the getItems function with fucnction createItemTable as a function. (it's passing a function as an argument) */
+    refreshList();
 });
 
 
-var x = setInterval(function() {
-       // Set the date we're counting down to
-
-       var countDownDate = dateToCountdownFrom.getTime(); /** would need to set this to whichever date entered for each item by the user (so would need to make this the user input box on the other page (input id="todoItemDate") i think */
-        alert(dateToCountdownFrom)
-
-      // Get today's date and time since this will be used to determine the time difference between now and the deadline
-      var now = new Date().getTime();
-
-      // Find the distance between now and the count down date
-      var distance = countDownDate - now;
-
-      // Time calculations for just days (thought having the seconds countdown would be a bit intense on a to-do list)
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24)); // Converts distance into days and rounds
-
-      if (days > 2) {
-        // If the deadline is more than 2 days away then just show the number of days
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = days + " days until deadline"
-
-      }
-      else if (days >= 0 && days <= 2){
-        // When the deadline is within 2 days away then display deadline approaching in orange
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "<span style='color: orange;'>Deadline approaching!</span>";
-
-      }
-      else if (days < 0){
-        // when the deadline is reached then it displays EXPIRED in red
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "<span style='color: red;'>DEADLINE EXPIRED</span>";
-      }
 
 
-
-    }, 1000);
 
---
+++ script1.js
@@ -0,0 +1,42 @@
+alert("hello");
+
+var countDownDate = dateToCountdownFrom.getTime(); /** would need to set this to whichever date entered for each item by the user (so would need to make this the user input box on the other page (input id="todoItemDate") i think */
+alert("hello");
+
+var x = setInterval(function() {
+    // Set the date we're counting down to
+    alert("hello");
+
+
+    // Get today's date and time since this will be used to determine the time difference between now and the deadline
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+
+    // Time calculations for just days (thought having the seconds countdown would be a bit intense on a to-do list)
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24)); // Converts distance into days and rounds
+
+    if (days > 2) {
+        // If the deadline is more than 2 days away then just show the number of days
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = days + " days until deadline"
+
+    }
+    else if (days >= 0 && days <= 2){
+        // When the deadline is within 2 days away then display deadline approaching in orange
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "<span style='color: orange;'>Deadline approaching!</span>";
+
+    }
+    else if (days < 0){
+        // when the deadline is reached then it displays EXPIRED in red
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "<span style='color: red;'>DEADLINE EXPIRED</span>";
+    }
+
+
+
+}, 1000);
+
