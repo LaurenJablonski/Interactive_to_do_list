bc2e7deda426c8929ff0d3f125ef6ad21e7ac545
LaurenJablonski <44180505+LaurenJablonski@users.noreply.github.com>
2020-10-18 20:50:21 +0100

    think got the CORS policy working

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const http = require('http'); // you get the http library in node.js. Note you h
 const fs = require('fs');// this allows us to read another file in our code. this variable fs allows us to do all the file handling that we need to do
 
 
-//CORS policy
-Access-Control-Allow-Origin: 'https://localhost:8080';
-Access-Control-Allow-Credentials: true //think this is cookies or authentification
-Access-Contol-Allow-Methods: GET
-Access-Control-Allow-Headers: Content-Type // so this means that https://localhost:8080 is allowed to make a GET request
-
-
-
 const server = http.createServer((request,response) => {//create a server using the http library you just imported and call the create server function on this object. The create server function takes a function that has 2 parameters, request and response which is going to handle all the activity on our server. SO everytime someone requests a page on our server, it is going to call this function.
 
     const { method, url, headers} = request; // in order to make a request you need these 3 parameters
     console.log( method, url, headers);
-    if (method === 'GET' && url === "/items"){ //use the response object passed into this function that gives the user a response
+    if (method === 'GET' && url === "/"){ //use the response object passed into this function that gives the user a response
         response.statuscode = 200 //tells the user that the status is okay
         response.setHeader("Content-Type", "application/json") //tells the browser we'll be writing in ("key", "value we want to set to that header"). tells it that the information being passed to it is in json, and so it should pass it as json.
+
+        //CORS policy
+
+        response.setHeader("Access-Control-Allow-Credentials", "true") //think this is cookies or authentification
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type")
+        response.setHeader("Access-Control-Allow-Methods", "GET") // so this means that https://localhost:8080 is allowed to make a GET request
+
 
         fs.readFile('index.html', function(error,data){
             if (error){
