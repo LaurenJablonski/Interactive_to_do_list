16c49815fe823abfea2386c073138ec37449576c
LaurenJablonski <44180505+LaurenJablonski@users.noreply.github.com>
2020-10-19 21:01:32 +0100

    changes to try and get the dictionary to display on my html page
    so in the response.setHeader the content type is  the thing that you directly reading, which from the POV of the server.js this is the idnex.html which is why index.html is needed instead of the application/json

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const server = http.createServer((request,response) => {//create a server using 
     console.log( method, url, headers);
     if (method === 'GET' && url === "/"){ //use the response object passed into this function that gives the user a response
         response.statuscode = 200 //tells the user that the status is okay
-        response.setHeader("Content-Type", "application/json") //tells the browser we'll be writing in ("key", "value we want to set to that header"). tells it that the information being passed to it is in json, and so it should pass it as json.
+        response.setHeader("Content-Type", "index.html") //tells the browser we'll be writing in ("key", "value we want to set to that header"). tells it that the information being passed to it is in json, and so it should pass it as json.
 
         //CORS policy
 
         response.setHeader("Access-Control-Allow-Credentials", "true") //think this is cookies or authentification
-        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Origin", "*") // allows any origin to access the server
         response.setHeader("Access-Control-Allow-Headers", "Content-Type")
         response.setHeader("Access-Control-Allow-Methods", "GET") // so this means that https://localhost:8080 is allowed to make a GET request
 
@@ -22,58 +22,55 @@ const server = http.createServer((request,response) => {//create a server using 
             if (error){
                 response.statuscode = 404 // 404 just means that we are not able to find what you are looking for
                 response.write('Error: File not found.')
-            } else{
+            } else {
                 response.write(data)//this will just write all the information that is in html
+
+                let dictionary = {
+                    "items": [
+
+                        {
+                            "ID": 1,
+                            "Name": "Walk the dog",
+                            "Desc": "25 min walk",
+                            "DueDate": "25/09/2020"
+                        },
+                        {
+                            "ID": 2,
+                            "Name": "Go to shops",
+                            "Desc": "buy bread",
+                            "DueDate": "14/09/2020"
+
+                        },
+                        {
+                            "ID": 3,
+                            "Name": "give presentation",
+                            "Desc": "next jump",
+                            "DueDate": "5/10/2020"
+
+                        }
+                    ]
+                }
+
+                const responseBody = {
+                    headers,
+                    method,
+                    url,
+                    //body: dictionary
+                }
+
+                response.write(JSON.stringify(responseBody))
             }
 
+            response.end(); //end the response
 
-        });
-
-        let dictionary = {
-            "items":[
-
-            {
-                "ID":1,
-                "Name": "Walk the dog",
-                "Desc": "25 min walk",
-                "DueDate": "25/09/2020"
-            },
-            {
-                "ID":2,
-                "Name": "Go to shops",
-                "Desc": "buy bread",
-                "DueDate": "14/09/2020"
-
-            },
-            {
-                "ID":3,
-                "Name": "give presentation",
-                "Desc": "next jump",
-                "DueDate": "5/10/2020"
-
-            }
-        ]
-    }
-
-    const responseBody = {
-    headers,
-    method,
-    url,
-    body: dictionary
-    }
-
-    response.write(JSON.stringify(responseBody))
-    response.end() //end the response
-
-}
-})
+})};
 
 server.listen(8080,function(error){//tells the server to listen on port 8080
     if (error){
         console.log('something went wrong', error)
     }else{
         console.log('server is listening on port 8080')
-    }})
+    }});
 
 
 
@@ -128,63 +125,5 @@ server.listen(8080,function(error){//tells the server to listen on port 8080
 
 
 
-//const sqlite3 = require('sqlite3').verbose(); //this imports the SQLite3 module. Verbose is the execution mode to produce long stack traces.
 
-//let db = new sqlite3.Database(':memory:', (err) => {//create a database object which opens the database connection automatically
-    //if (err) {
-      //  return console.error(err.message);//here we're using a callback function that executes an error message if there's an error, but if the connection is successful then this will be stated in the console.
-    //}
-  //  console.log('Connected to the in-memory SQlite database.');
-//});
-
-//db.close((err) => {//this closes the database once the pending queries have been completed again using callback functions
-    //if (err) {
-      //  return console.error(err.message);
-    //}
-  //  console.log('Close the database connection.');
-//});
-
-
-
-
-
-
-
-
-//var http = require('http');
-
-//const PORT = 3000; // Non-standard HTTP port. Note a standard port would be port 80
-
-
-//function handleRequest(request, response) {
-  //  console.log(request.headers);
-    //response.end('It Works!! Path Hit: ' + request.url);
-//}
-
-//var server = http.createServer(handleRequest);
-//server.listen(PORT, function () {
-  //  console.log("Server listening on: http://localhost:%s", PORT);
-//});
-
-
-
-
-
-
-
-
-//const http = require('http');
-
-//const hostname = '127.0.0.1';
-//const port = 3000;
-
-//const server = http.createServer((req, res) => {
-  //  res.statusCode = 200;
-    //res.setHeader('Content-Type', 'text/plain');
-    //res.end('Hello, World!\n');
-//});
-
-//server.listen(port, hostname, () => {
-  //  console.log(`Server running at http://${hostname}:${port}/`);
-//});
 
