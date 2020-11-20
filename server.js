4a9e09f26d64bda99120f969bb4461c4b3d60fac
LaurenJablonski <jablonski.lauren1@gmail.com>
2020-11-20 01:43:15 +0000

    still trying to display dummy items

--- server.js
+++ server.js
@@ -9,105 +9,95 @@ const fs = require('fs');// this allows us to read another file in our code. thi
 const server = http.createServer((request,response) => {//create a server using the http library you just imported and call the create server function on this object. The create server function takes a function that has 2 parameters, request and response which is going to handle all the activity on our server. SO everytime someone requests a page on our server, it is going to call this function.
 
     const {headers, method, url} = request; //this request object is an instant of an Incoming Message
-    //console.log(request.method); having this here tells you what the original request is and it is OPTIONS
+    console.log(request.method); //having this here tells you what the original request is and it is OPTIONS
 
 
     // assigns values to the headers
     if (request.method === 'OPTIONS' && request.url === '/item') {
         console.log("PREFLIGHT OPTIONS REQUEST")
-
-        // checks the request is preflight
-
-        var isPreflight = function(request) { // this detects if the incoming request is a preflight request. It checks if the request is an OPTIONS request, if the request has an origin header and if is has a Access-Control-Request-Method header
-
-            var isHttpOptions = request.method === 'OPTIONS';
-            var hasOriginHeader = request.headers['origin'] === '*';
-            var hasRequestMethod = request.headers['access-control-request-method']
-            //var hasOriginHeader = request.headers === '*';
-            //var hasRequestMethod = request.headers === 'access-control-request-method';// this header asks the server for permission to make a request using a certain HTTP method
-            return isHttpOptions && hasOriginHeader && hasRequestMethod;
-
-        };
-        console.log("hello");
-       console.log(isPreflight(request));
-       console.log(request.headers);
+        console.log(request.headers);
 
         //then handles the preflight request
 
-         var handleCors = function(request, response){
-             response.setHeader('Access-Control-Allow-Origin', '*'); // allows any origin to access the server
-             if (isPreflight(request)){
-                 console.log("its a preflight request");
-                 response.setHeader('Access-Control-Allow-Headers', '*');
-                 response.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-                 response.setHeader('Content-Type', 'text/html');
-                 response.statuscode = 404;
-                 response.end();
-                 return;
+        var handleCors = function (request, response) {
+            console.log("its a preflight request");
+            response.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080'); // allows any origin to access the server
+            response.setHeader('Access-Control-Allow-Headers', '*');
+            response.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+            response.setHeader('Content-Type', 'text/html');
+            response.statuscode = 404;
+            response.end();
+            //console.log(response.headers);
+            return response; //doing return response shows you the server response
+        }
+        handleCors(request,response); //Without this the initial request is OPTIONS, but with it the initial request is GET
 
-             } else{
-                 console.log("no this is not a preflight request");
-             }
+          if (handleCors('GET', response)){
+         //if (request.method === 'GET' && request.url === '/item'){
+             console.log("hello world");
+             fs.readFile('index.html', function (error, data) {
+                 if (error) {
+                     response.statuscode = 404 // 404 just means that we are not able to find what you are looking for
+                     response.write('Error: File not found.')
+                 } else {
 
-         }
+                     let dictionary = {
+                         "items": [
 
-    console.log(handleCors(request,response))};
+                             {
+                                 "ID": 1,
+                                 "Name": "Walk the dog",
+                                 "Desc": "25 min walk",
+                                 "DueDate": "25/09/2020"
+                             },
+                             {
+                                 "ID": 2,
+                                 "Name": "Go to shops",
+                                 "Desc": "buy bread",
+                                 "DueDate": "14/09/2020"
 
+                             },
+                             {
+                                 "ID": 3,
+                                 "Name": "give presentation",
+                                 "Desc": "next jump",
+                                 "DueDate": "5/10/2020"
 
+                             }
+                         ]
+                     }
 
+                     response.write(data)//this will just write all the information that is in html
 
-    if (method === 'GET' && url === '/item'){
-        fs.readFile('index.html', function (error, data) {
-            if (error) {
-                response.statuscode = 404 // 404 just means that we are not able to find what you are looking for
-                response.write('Error: File not found.')
-            } else {
+                     const responseBody = {
+                         //headers,
+                         //method,
+                         //url,
+                         body: dictionary
+                     }
 
-                let dictionary = {
-                    "items": [
+                     response.write(JSON.stringify(responseBody))
+                     //response.write(dictionary[2]['DueDate'])
+                     //console.log('dictionary'['Name'])
+                     console.log(responseBody);
+                 }
 
-                        {
-                            "ID": 1,
-                            "Name": "Walk the dog",
-                            "Desc": "25 min walk",
-                            "DueDate": "25/09/2020"
-                        },
-                        {
-                            "ID": 2,
-                            "Name": "Go to shops",
-                            "Desc": "buy bread",
-                            "DueDate": "14/09/2020"
-
-                        },
-                        {
-                            "ID": 3,
-                            "Name": "give presentation",
-                            "Desc": "next jump",
-                            "DueDate": "5/10/2020"
-
-                        }
-                    ]
-                }
-
-                response.write(data)//this will just write all the information that is in html
-
-                const responseBody = {
-                    //headers,
-                    //method,
-                    //url,
-                    body: dictionary
-                }
-
-                response.write(JSON.stringify(responseBody))
-                //response.write(dictionary[2]['DueDate'])
-                //console.log('dictionary'['Name'])
-                console.log(responseBody);
-            }
-
-            response.end(); //end the response
+                 response.end(); //end the response
+                 return responseBody;
 
 
-        })}});
+
+
+         })
+    }else{
+              console.log("it's not a GET request");
+          }}
+
+
+
+
+
+    });
 
 
 
